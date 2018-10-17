a0c52b11a54e9c5e7d82e4d19d4973d7943a8aad
Dench8833 <chbksrv@gmail.com>
2018-10-17 16:22:14 +0300

    Create clock.js

---
+++ clock.js
@@ -0,0 +1,8 @@
+function jsClocK() {
+	var date = new Date();
+	var d = date.toLocaleDateString();
+	var t = date.toLocaleTimeString();
+	var ms = date.getUTCMilliseconds();
+	document.getElementById("clockJs").innerHTML = d + ' ' + t + ':' + ms;
+}
+setInterval(jsClocK, 10);
